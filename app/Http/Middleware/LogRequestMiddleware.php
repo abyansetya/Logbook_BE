d561576ae1e5648dcb6bdd2ad1b0fe41aef44ced
abyansetya <abyansetyaa@gmail.com>
2026-02-03 23:21:06 +0700

    be: add log for monitoring

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $startTime = microtime(true);
+
+        $response = $next($request);
+
+        $duration = round((microtime(true) - $startTime) * 1000, 2); // ms
+
+        $user = Auth::user();
+        $userId = $user ? $user->id : 'Guest';
+        $method = $request->method();
+        $url = $request->path();
+        $ip = $request->ip();
+        $status = $response->getStatusCode();
+
+        // Format: [REQUEST] METHOD URL | User: ID | IP: x.x.x.x | Duration: Xms | Status: SSS
+        Log::info("[REQUEST] {$method} {$url} | User: {$userId} | IP: {$ip} | Duration: {$duration}ms | Status: {$status}");
+
+        return $response;
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,7 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -31,6 +32,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+        
+        // Log every request
+        $middleware->append(\App\Http\Middleware\LogRequestMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

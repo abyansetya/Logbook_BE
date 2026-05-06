22b47f6bb9f264569656d67d7869dee088ae6595
abyansetya <abyansetyaa@gmail.com>
2026-05-06 20:15:05 +0700

    Implement soft delete for users

--- app/Models/ActivityLogs.php
+++ app/Models/ActivityLogs.php
@@ -15,6 +15,6 @@ class ActivityLogs extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
--- app/Models/Log.php
+++ app/Models/Log.php
@@ -22,7 +22,7 @@ class Log extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function mitra()
